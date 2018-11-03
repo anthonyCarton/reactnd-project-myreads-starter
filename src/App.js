ea2135b072b5b1e1f3653d3131a6f62ebaf9878c
Anthony Carton <mr.carton@gmail.com>
2018-11-02 20:00:34 -0600

    refactor: Add exact to path

--- src/App.js
+++ src/App.js
@@ -6,23 +6,13 @@ import Home from './Home'
 import Search from './Search'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
   render() {
     return (
 			<div className="app">
         <Route exact path='/' render={() => (
           <Home />
         )}/>
-        <Route path='/search' render={({ history }) => (
+        <Route exact path='/search' render={({ history }) => (
           <Search />
         )}/>
 			</div>
